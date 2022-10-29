80cb8a7877e34ec0f8c205f1725059a600ccfe0d
WuKong <111810384+Stanwukong@users.noreply.github.com>
2022-10-29 11:58:30 +0100

    Generate

--- main.js
+++ main.js
@@ -1,26 +1,78 @@
-class Color {
-  	constructor(hex, element) {
-		this.hex = hex;
-		this.element = element;
+function newColorA() {
+	let symbols, color;
+	symbols = "0123456789ABCDEF";
+
+	color = "#";
+
+	for(let i = 0; i < 6; i++){
+		color = color + symbols[Math.ceil(Math.random() * 16)]
+	}
+	document.getElementById('first').style.background = color;
+	document.getElementById('A').innerHTML = color;
+}
+function newColorB() {
+	let symbols, color;
+	symbols = "0123456789ABCDEF";
+
+	color = "#";
+
+	for(let i = 0; i < 6; i++){
+		color = color + symbols[Math.floor(Math.random() * 16)]
+	}
+	document.getElementById('second').style.background = color;
+	document.getElementById('B').innerHTML = color;
+}
+function newColorC() {
+	let symbols, color;
+	symbols = "0123456789ABCDEF";
+
+	color = "#";
+
+	for(let i = 0; i < 6; i++){
+		color = color + symbols[Math.floor(Math.random() * 16)]
+	}
+	document.getElementById('rd').style.background = color;
+	document.getElementById('C').innerHTML = color;
+}
+function newColorD() {
+	let symbols, color;
+	symbols = "0123456789ABCDEF";
+
+	color = "#";
+
+	for(let i = 0; i < 6; i++){
+		color = color + symbols[Math.floor(Math.random() * 16)]
 	}
 
-	setHex (hex) {
-		this.hex = hex;
-		this.element.style.backgroundColor = hex;
-		this.element.querySelector('.box').value = hex;
-	}
-
-	generateHex () {
-		const chars = '0123456789ABCDEF';
-		let hex = '#';
-		for (let i = 0; i < 6; i++) {
-			hex += chars[Math.floor(Math.random() * 16)];
-		}
-
-		this.setHex(hex);
-
-		return hex;
-
-	}
+	document.getElementById('th').style.background = color;
+	document.getElementById('D').innerHTML = color;
+}
+function newColorE() {
+	let symbols, color;
+	symbols = "0123456789ABCDEF";
+	
+	color = "#";
+	
+	for(let i = 0; i < 6; i++){
+		color = color + symbols[Math.ceil(Math.random() * 16)]
+	}	
+	document.getElementById('tj').style.background = color;
+	document.getElementById('E').innerHTML = color;
 }
 
+function newColor() {
+	newColorA();
+	newColorB();
+	newColorC();
+	newColorD();
+	newColorE();
+
+}
+
+const keyBoard = document.querySelector('keyBoard');
+
+keyBoard.addEventListener('keydown', (e) => {
+	if (e.composed.toLowerCase() === 'Space') {
+		newColor();
+	}
+})
